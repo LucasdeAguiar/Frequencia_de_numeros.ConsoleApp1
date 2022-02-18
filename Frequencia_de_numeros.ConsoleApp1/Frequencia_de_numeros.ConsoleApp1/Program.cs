6d32720b2ac57028bc66ce4ddf38ebc97aed704d
LucasdeAguiar <80419489+LucasdeAguiar@users.noreply.github.com>
2022-02-18 16:28:46 -0300

    Add project files.

---
+++ Frequencia_de_numeros.ConsoleApp1/Frequencia_de_numeros.ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Frequencia_de_numeros.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quantos valores será a lista de números");
+             int qtnumero = System.Convert.ToInt32(Console.ReadLine());
+             int numeroanterior=0;
+
+            int [] numeros = new int[qtnumero];
+            int[] contador = new int[qtnumero];        
+
+            for (int i=0; i<numeros.Length;i++) {
+                Console.WriteLine("Digite os valor na posição  " + i);
+                 numeros[i] = System.Convert.ToInt32(Console.ReadLine());
+
+                if (numeros[i] == 0)
+                {
+                    numeroanterior = numeros[i];
+                }
+                else if (i > 0)
+
+                    if (numeros[i] == numeroanterior)
+                    {
+                        numeroanterior = numeros[i];
+                        contador[i]++;
+                    }
+               
+            }
+
+
+
+
+
+            //Mostrar a lista
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i] + " aparece " + contador[i] + " vez(es)");
+            }
+		}
+    }
+}
